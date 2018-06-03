477ad66b46f6bd433641ed1f45b5fe1b5015c3ee
Kristian JT Woods <kris_woods@hotmail.co.uk>
2018-06-03 18:27:10 +0100

    added plain js for img carousel

---
+++ main.js
@@ -0,0 +1,57 @@
+let sliderImages = document.querySelectorAll('.slide');
+let arrowLeft = document.querySelector('#arrowLeft');
+let arrowRight = document.querySelector('#arrowRight');
+let current = 0;
+
+
+// clear all images
+
+function reset() {
+    for(let i = 0; i < sliderImages.length; i++) {
+        sliderImages[i].style.display = 'none';
+    }
+}
+
+
+// init slider
+
+function startSlide() {
+    reset();
+    sliderImages[0].style.display = 'block';
+}
+
+// show previous
+
+function slideLeft() {
+    reset();
+    sliderImages[current - 1].style.display = 'block';
+    current--;
+}
+
+// show next
+
+function slideRight() {
+    reset();
+    sliderImages[current + 1].style.display = 'block';
+    current++;
+}
+
+// left arrow click 
+
+arrowLeft.addEventListener('click', () => {
+    if(current === 0) {
+        current = sliderImages.length;
+    }
+    slideLeft();
+});
+
+// right arrow click
+
+arrowRight.addEventListener('click', () => {
+    if(current === sliderImages.length - 1) {
+        current = -1;
+    }
+    slideRight();
+});
+
+startSlide();
